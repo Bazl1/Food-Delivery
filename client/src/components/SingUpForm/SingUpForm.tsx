8e4a3f45bff792d13ef034dfde6ec684a482939c
Bazl1 <dazman3243243@gmail.com>
2024-01-25 20:46:32 +0200

    update

--- client/src/components/SingUpForm/SingUpForm.tsx
+++ client/src/components/SingUpForm/SingUpForm.tsx
@@ -4,6 +4,8 @@ import s from "./SingUpForm.module.scss";
 import { useState } from "react";
 import { useMutation } from "@apollo/client";
 import { SIGN_UP_AS_CUSTOMER } from "../../graphql/RegistrationCustomer.mutation";
+import toast, { Toaster } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 interface IForm {
     email: string;
@@ -16,7 +18,9 @@ const SingUpForm = () => {
     const [name, setName] = useState<string>("");
     const [password, setPassword] = useState<string>("");
 
-    const [signUpAsCustomer, { error }] = useMutation(SIGN_UP_AS_CUSTOMER, {
+    const navigate = useNavigate();
+
+    const [signUpAsCustomer] = useMutation(SIGN_UP_AS_CUSTOMER, {
         onCompleted: (data) => {
             if (
                 data.signUpAsCustomer?.accessToken !== null &&
@@ -24,6 +28,10 @@ const SingUpForm = () => {
             ) {
                 localStorage.setItem("token", data.signUpAsCustomer?.accessToken);
             }
+        },
+        onError(error) {
+            toast.error(error.message);
+            throw new Error(error.message);
         },
     });
 
@@ -43,73 +51,74 @@ const SingUpForm = () => {
                 userName: name,
             },
         });
-        if (error) {
-            throw new Error(error.message);
-        }
+        navigate("/");
     };
 
     return (
-        <form className={s.form} onSubmit={handleSubmit(Submit)}>
-            <Input
-                text="Your email"
-                type="email"
-                name="email"
-                value={email}
-                setValue={setEmail}
-                register={register}
-                errors={errors}
-                validationOptions={{
-                    required: "Required field",
-                    pattern: {
-                        value: /^((([0-9A-Za-z]{1}[-0-9A-z\.]{1,}[0-9A-Za-z]{1})|([0-9А-Яа-я]{1}[-0-9А-я\.]{1,}[0-9А-Яа-я]{1}))@([-A-Za-z]{1,}\.){1,2}[-A-Za-z]{2,})$/u,
-                        message: "Enter a valid email",
-                    },
-                }}
-            />
-            <Input
-                text="Your name"
-                type="text"
-                name="name"
-                value={name}
-                setValue={setName}
-                register={register}
-                errors={errors}
-                validationOptions={{
-                    required: "Required field",
-                    minLength: {
-                        value: 2,
-                        message: "Minimum 2 characters",
-                    },
-                    maxLength: {
-                        value: 32,
-                        message: "Maximum number of characters 32",
-                    },
-                }}
-            />
-            <Input
-                text="Your password"
-                type="password"
-                name="password"
-                value={password}
-                setValue={setPassword}
-                register={register}
-                errors={errors}
-                validationOptions={{
-                    required: "Required field",
-                    minLength: {
-                        value: 8,
-                        message: "Minimum password length 8 characters",
-                    },
-                    maxLength: {
-                        value: 21,
-                        message: "Maximum password length 21 characters",
-                    },
-                }}
-            />
-            <button className={`${s.form__btn} btn-style-one`} type="submit">
-                Register
-            </button>
-        </form>
+        <>
+            <form className={s.form} onSubmit={handleSubmit(Submit)}>
+                <Input
+                    text="Your email"
+                    type="email"
+                    name="email"
+                    value={email}
+                    setValue={setEmail}
+                    register={register}
+                    errors={errors}
+                    validationOptions={{
+                        required: "Required field",
+                        pattern: {
+                            value: /^((([0-9A-Za-z]{1}[-0-9A-z\.]{1,}[0-9A-Za-z]{1})|([0-9А-Яа-я]{1}[-0-9А-я\.]{1,}[0-9А-Яа-я]{1}))@([-A-Za-z]{1,}\.){1,2}[-A-Za-z]{2,})$/u,
+                            message: "Enter a valid email",
+                        },
+                    }}
+                />
+                <Input
+                    text="Your name"
+                    type="text"
+                    name="name"
+                    value={name}
+                    setValue={setName}
+                    register={register}
+                    errors={errors}
+                    validationOptions={{
+                        required: "Required field",
+                        minLength: {
+                            value: 2,
+                            message: "Minimum 2 characters",
+                        },
+                        maxLength: {
+                            value: 32,
+                            message: "Maximum number of characters 32",
+                        },
+                    }}
+                />
+                <Input
+                    text="Your password"
+                    type="password"
+                    name="password"
+                    value={password}
+                    setValue={setPassword}
+                    register={register}
+                    errors={errors}
+                    validationOptions={{
+                        required: "Required field",
+                        minLength: {
+                            value: 8,
+                            message: "Minimum password length 8 characters",
+                        },
+                        maxLength: {
+                            value: 21,
+                            message: "Maximum password length 21 characters",
+                        },
+                    }}
+                />
+                <button className={`${s.form__btn} btn-style-one`} type="submit">
+                    Register
+                </button>
+            </form>
+            <Toaster position="bottom-left" reverseOrder={false} />
+        </>
     );
 };
 
--- client/src/components/SingUpFormRestaurant/SingUpFormRestaurant.tsx
+++ client/src/components/SingUpFormRestaurant/SingUpFormRestaurant.tsx
@@ -4,6 +4,8 @@ import s from "./SingUpFormRestaurant.module.scss";
 import { useState } from "react";
 import { useMutation } from "@apollo/client";
 import { SIGN_UP_AS_RESTAURANT } from "../../graphql/RegistrationRestaurant.mutation";
+import toast, { Toaster } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 interface IForm {
     email: string;
@@ -18,7 +20,9 @@ const SingUpFormRestaurant = () => {
     const [password, setPassword] = useState<string>("");
     const [description, setDescription] = useState<string>("");
 
-    const [signUpAsRestaurant, { error }] = useMutation(SIGN_UP_AS_RESTAURANT, {
+    const navigate = useNavigate();
+
+    const [signUpAsRestaurant] = useMutation(SIGN_UP_AS_RESTAURANT, {
         onCompleted: (data) => {
             if (
                 data.signUpAsRestaurant?.accessToken !== null &&
@@ -26,6 +30,10 @@ const SingUpFormRestaurant = () => {
             ) {
                 localStorage.setItem("token", data.signUpAsRestaurant?.accessToken);
             }
+        },
+        onError(error) {
+            toast.error(error.message);
+            throw new Error(error.message);
         },
     });
 
@@ -46,93 +54,94 @@ const SingUpFormRestaurant = () => {
                 description: description,
             },
         });
-        if (error) {
-            throw new Error(error.message);
-        }
+        navigate("/");
     };
 
     return (
-        <form className={s.form} onSubmit={handleSubmit(Submit)}>
-            <Input
-                text="Your email"
-                type="email"
-                name="email"
-                value={email}
-                setValue={setEmail}
-                register={register}
-                errors={errors}
-                validationOptions={{
-                    required: "Required field",
-                    pattern: {
-                        value: /^((([0-9A-Za-z]{1}[-0-9A-z\.]{1,}[0-9A-Za-z]{1})|([0-9А-Яа-я]{1}[-0-9А-я\.]{1,}[0-9А-Яа-я]{1}))@([-A-Za-z]{1,}\.){1,2}[-A-Za-z]{2,})$/u,
-                        message: "Enter a valid email",
-                    },
-                }}
-            />
-            <Input
-                text="Your restaurant name"
-                type="text"
-                name="name"
-                value={name}
-                setValue={setName}
-                register={register}
-                errors={errors}
-                validationOptions={{
-                    required: "Required field",
-                    minLength: {
-                        value: 2,
-                        message: "Minimum 2 characters",
-                    },
-                    maxLength: {
-                        value: 32,
-                        message: "Maximum number of characters 32",
-                    },
-                }}
-            />
-            <Input
-                text="Your password"
-                type="password"
-                name="password"
-                value={password}
-                setValue={setPassword}
-                register={register}
-                errors={errors}
-                validationOptions={{
-                    required: "Required field",
-                    minLength: {
-                        value: 8,
-                        message: "Minimum password length 8 characters",
-                    },
-                    maxLength: {
-                        value: 21,
-                        message: "Maximum password length 8 characters",
-                    },
-                }}
-            />
-            <Input
-                text="Restaurant description"
-                type="text"
-                name="description"
-                value={description}
-                setValue={setDescription}
-                register={register}
-                errors={errors}
-                validationOptions={{
-                    required: "Required field",
-                    minLength: {
-                        value: 10,
-                        message: "Minimum password length 8 characters",
-                    },
-                    maxLength: {
-                        value: 32,
-                        message: "Maximum password length 8 characters",
-                    },
-                }}
-            />
-            <button className={`${s.form__btn} btn-style-one`} type="submit">
-                Register
-            </button>
-        </form>
+        <>
+            <form className={s.form} onSubmit={handleSubmit(Submit)}>
+                <Input
+                    text="Your email"
+                    type="email"
+                    name="email"
+                    value={email}
+                    setValue={setEmail}
+                    register={register}
+                    errors={errors}
+                    validationOptions={{
+                        required: "Required field",
+                        pattern: {
+                            value: /^((([0-9A-Za-z]{1}[-0-9A-z\.]{1,}[0-9A-Za-z]{1})|([0-9А-Яа-я]{1}[-0-9А-я\.]{1,}[0-9А-Яа-я]{1}))@([-A-Za-z]{1,}\.){1,2}[-A-Za-z]{2,})$/u,
+                            message: "Enter a valid email",
+                        },
+                    }}
+                />
+                <Input
+                    text="Your restaurant name"
+                    type="text"
+                    name="name"
+                    value={name}
+                    setValue={setName}
+                    register={register}
+                    errors={errors}
+                    validationOptions={{
+                        required: "Required field",
+                        minLength: {
+                            value: 2,
+                            message: "Minimum 2 characters",
+                        },
+                        maxLength: {
+                            value: 32,
+                            message: "Maximum number of characters 32",
+                        },
+                    }}
+                />
+                <Input
+                    text="Your password"
+                    type="password"
+                    name="password"
+                    value={password}
+                    setValue={setPassword}
+                    register={register}
+                    errors={errors}
+                    validationOptions={{
+                        required: "Required field",
+                        minLength: {
+                            value: 8,
+                            message: "Minimum password length 8 characters",
+                        },
+                        maxLength: {
+                            value: 21,
+                            message: "Maximum password length 8 characters",
+                        },
+                    }}
+                />
+                <Input
+                    text="Restaurant description"
+                    type="text"
+                    name="description"
+                    value={description}
+                    setValue={setDescription}
+                    register={register}
+                    errors={errors}
+                    validationOptions={{
+                        required: "Required field",
+                        minLength: {
+                            value: 10,
+                            message: "Minimum password length 8 characters",
+                        },
+                        maxLength: {
+                            value: 32,
+                            message: "Maximum password length 8 characters",
+                        },
+                    }}
+                />
+                <button className={`${s.form__btn} btn-style-one`} type="submit">
+                    Register
+                </button>
+            </form>
+            <Toaster position="bottom-left" reverseOrder={false} />
+        </>
     );
 };
 
