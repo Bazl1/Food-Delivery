7f5e21c9d050fad68aaecd5ebd72617b427bb4ec
Bazl1 <dazman3243243@gmail.com>
2024-02-04 16:46:38 +0200

    update

--- client/src/graphql/Search.query.ts
+++ client/src/graphql/Search.query.ts
@@ -1,8 +1,8 @@
 import { gql } from "../__generated__/gql";
 
 export const SEARCH = gql(`
-    query searchOnRestaurant($page: Int!, $limit: Int!, $predicate: String, $restaurantId: String){
-        search(page: $page, limit: $limit, predicate: $predicate, restaurantId: $restaurantId){
+    query searchOnRestaurant($page: Int!, $limit: Int!, $predicate: String, $restaurantId: String, $filtering: String, $categories: String){
+        search(page: $page, limit: $limit, predicate: $predicate, restaurantId: $restaurantId, filtering: $filtering, categories: $categories){
             products{
                 id
                 picture
